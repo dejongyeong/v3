d3e0a0758d8d602501e722ff20aa8d0a1a4b2575
dejongyeong <dejong.re2@gmail.com>
2025-03-01 23:41:10 +0000

    feat: add bundler analyzer to investigate large dependencies

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,24 @@ import { fileURLToPath } from "node:url";
 
 import { createJiti } from "jiti";
 import type { NextConfig } from "next";
+import withBundleAnalyzer from "@next/bundle-analyzer";
 
 const jiti = createJiti(fileURLToPath(import.meta.url));
 
 // validate env on build
 jiti.esmResolve("./src/env.ts");
 
+// bundle analyzer
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  experimental: {
+    optimizePackageImports: [],
+  },
 };
 
-export default nextConfig;
+export default bundleAnalyzer(nextConfig);
